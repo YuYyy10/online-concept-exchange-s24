2dd3d564f6938c2ff5346f9655660894573aa783
Ryan <96708796+rzearley7@users.noreply.github.com>
2024-03-22 03:23:59 -0400

    Removing duplicate changes, trying a route

--- oce/content/routes.py
+++ oce/content/routes.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
+from flask import render_template
 
 content = Blueprint('content', __name__)
+
+@content.route("/content/block1")
+def block1():
+  render_template("block1.html")
