578e149e1d6f5ba21f4da846d62cfe74cc41fd3a
CoachSquish <73550493+CoachSquish@users.noreply.github.com>
2024-11-13 16:19:51 -0500

    Test cases

--- oce/__init__.py
+++ oce/__init__.py
@@ -4,8 +4,9 @@ from argon2 import PasswordHasher
 from flask import Flask
 from flask_login import LoginManager
 
-#login_manager = LoginManager()
-#password_hasher = PasswordHasher()
+
+login_manager = LoginManager()
+password_hasher = PasswordHasher()
 
 
 def create_app():
