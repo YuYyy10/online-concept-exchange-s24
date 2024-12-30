31c5a9afb723964c9ff9b9fbafa6a5fa2d3e7809
PastelKobold <swebster495@gmail.com>
2024-12-30 15:23:04 -0500

    fixing wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,10 @@
 from oce import create_app
-from flask_frozen import Freezer
 from flask import render_template
 
 app = create_app()
-freezer = Freezer(app)  # Initialize Freezer with the app
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    freezer.freeze()  # This will generate static files for Netlify
+  return render_template('index.html')
