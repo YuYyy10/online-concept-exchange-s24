8eb275789120c9c3dc096b032ed0f83d2decfa09
PastelKobold <swebster495@gmail.com>
2024-12-31 22:14:13 -0500

    Fixed spelling mistakes

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,15 @@
 from oce import create_app
 from flask import render_template
+#from flask_limiter import Limiter
+#from flask_limiter.util import get_remote_address
 
 app = create_app()
+#limiter = Limiter(get_remote_address, app=app)
+
 if __name__ == '__main__':
     app.run()
 
 @app.route('/')
+#@limiter.limit("5/minute")
 def home():
   return render_template('index.html')
